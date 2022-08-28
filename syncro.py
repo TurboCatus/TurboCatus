54e6e0981afb9301ac82839ae60da973c1940673
TurboCatus <100853283+TurboCatus@users.noreply.github.com>
2022-08-28 17:52:09 +0700

    Cynro to phone

---
+++ syncro.py
@@ -0,0 +1,26 @@
+#! /usr/bin/python3
+import os
+p=os.system('ping -c 3 192.168.1.5 > ping.txt') #ping ip address
+#log=os.system('touch log_sync.txt')
+a=open('ping.txt')
+b=a.read()
+c=('ttl=') #if ping done
+p=('8022/tcp') # key word for test port
+# ss=os.system('nmap 192.168.1.3 > test_ssh.txt')
+port=os.system('nmap 192.168.1.5 > port.txt') # test port
+pp=open('port.txt')
+ppp=pp.read()
+if p in ppp:
+  os.system('echo port_open >> log_sync.txt')
+if c not in a:
+  os.system('echo host_not_reachable >> log_sync.txt|date >> log_sync.txt|echo ----- >> log_sync.txt') #test ping
+#if p not in port:
+#  os.system('echo port_not_open >> log_sync.txt|date >> log_sync.txt|echo----->> log_sync.txt') #test port
+ #проверка на отсутствие ошибок, на пустой файл с ошибками для подключения по ssh
+if (c in b) and (p in ppp):
+    os.system('echo all_done >> log_sync.txt|date >> log_sync.txt|echo ----- >> log_sync.txt') # log
+    os.system(' rsync -avzhe "ssh -p 8022" Linux@192.168.1.5:/data/data/com.termux/files/home/storage/shared/Android/media/com.whatsapp/WhatsApp/Media/ /media/lubuntu/cecdbebf-1288-4fcc-9e7e-c59f15bf5af8/Phone_back/ >> log_sync.txt 2>&1') #log syncro
+  #отправка отчета на почту
+else:
+    os.system('echo something_not_ok >> log_sync.txt|date >> log_sync.txt|echo ----- >> log_sync.txt')
+
