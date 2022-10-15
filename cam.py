9ee5bc4f1a21860ef624dc4f1390f4170e5fc2c8
TurboCatus <100853283+TurboCatus@users.noreply.github.com>
2022-10-15 18:37:28 +0700

    Cam shot
    Cam shot

---
+++ cam.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import os
+os.system('ls ./shot | grep .jpg  > ./shot/list.txt')  # List of jpeg files
+a=open('./shot/list.txt')
+b=a.read()
+c=b.split() # Split to file one by one
+print(c)
+l=len(c) # Who last
+print(l)
+os.system(f'fswebcam -r 640x480 --jpeg 85 -D 1 /home/lubuntu/to_test/Cam/shot/{l}.jpg  >> log_shot.txt 2>&1')
+os.system('date >> log_shot.txt')
+
+
