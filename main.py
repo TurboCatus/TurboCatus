2d3110a8812269e557a8df13234eec3fca4a21f8
TurboCatus <100853283+TurboCatus@users.noreply.github.com>
2022-10-24 12:57:51 +0700

    To_search_ip_add
    ADD_Telegramm_send

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-from netmiko import ConnectHandler
+'''from netmiko import ConnectHandler
 import os
-'''
 #1---Поиск vlan и mac адреса на коммутаторе и вышрузка в файл arp.txt
-port=('1/0/15')
+port=('1/0/14') #Номер порта коммутатора
 arp=open('arp.txt','w')
 switch = {'device_type': 'cisco_ios', 'ip': '192.168.3.135', 'username': 'root', 'password': 'root12345',
           'secret': 'root12345'}
 connect = ConnectHandler(**switch)
 #connect.enable()
 #print(connect.find_prompt())
-output = connect.send_command(f'show mac-addr-table interface {port}')
+output = connect.send_command(f'show mac-addr-table interface {port}') #команда на коммутаторе Qtech
 print(output)
 print(output,file=arp)
 arp.close()
@@ -21,26 +20,44 @@ Mac=mac_add.read()
 a=Mac.split()
 #print(a)
 print(a[9],file=Vlan) #позиция vlan в файле
-print(a[11],file=MAC) #позиция mac-адреса в файле(могут быть 2 mac-адреса)
-ch=a[11].replace(':','-') #замена с двоеточия на тире
+print(a[8],file=MAC) #позиция mac-адреса в файле(могут быть 2 mac-адреса),8 поз==1 MAC|11 поз==2 MAC
+ch=a[8].replace(':','-') #замена с двоеточия на тире(заменить на позицию MAC-адреса)
 c=ch.lower() #замена на нижний регистр
-#print(c)
+print(c)
 #3---Сканирование адресов в зависимости от номера Vlan.(Дописать автоматическое переключения vlan на коммутаторе )
 if a[9]=='102':
     os.system('cd "C:\\Program Files (x86)\\Nmap" | nmap -F 192.168.1.66-126 > arp_nmap.txt' )
     os.system(f'arp -a | findstr {c} > RAW_IP.txt')
+else:
+    print('Check VLAN')
 #elif a[9]=='101'
 #arp -a | findstr 08-8f-c3-09-0e-a1
 mac_add.close()
 MAC.close()
-Vlan.close()'''
+Vlan.close()
 #4---Вывод ip-адреса
 Raw_ip=open('Raw_IP.txt')
 ip_fin=open('IP.txt','w')
 ip=Raw_ip.read()
 ip_a=ip.split()
-print(ip_a[0],file=ip_fin)
+print(ip_a[0],file=ip_fin) #Позиция ip адреса в файле и запись в файл ip_fin.txt
 print(ip_a[0])
 Raw_ip.close()
 ip_fin.close()
+os.system(f'cd "C:\\Program Files (x86)\\Nmap" | nmap -O {ip_a[0]} > OS.txt')
+'''
+
+import telebot,time
+
+token = '5629862008:AAGbPMYXqcmthw-vW-tvyoUfdGkQ_Qdl_Gw'
+bot = telebot.TeleBot(token)
+chat_id = '1366665116'
+text = 'Next check'
+bot.send_message(chat_id, text)
+time.sleep(15)
+
+#bot.infinity_polling(timeout=20, long_polling_timeout = 10)
+
+
+
 
